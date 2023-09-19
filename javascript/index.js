2af83570b0cc073a56e1bef1d141b2ed913dd384
MuhammadNasir10 <nmian7080@gmail.com>
2023-09-19 18:24:00 +0500

    section complete

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 let appendNumber = 600;
 let prependNumber = 1;
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper(".swiper", {
   slidesPerView: 2,
   centeredSlides: true,
   spaceBetween: 30,
@@ -18,17 +18,37 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-let video = document.querySelector("#aboutvido");
-let playicon = document.querySelector(".about-video  .playicon");
-let a = 1;
-video.addEventListener("click", () => {
-  if (a == 1) {
-    video.play();
-    playicon.style.visibility = "hidden";
-    a = 0;
-  } else {
-    video.pause();
-    playicon.style.visibility = "visible";
-    a = 1;
-  }
-});
+
+// var swiper = new Swiper(".slide-container", {
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   slidesPerGroup: 4,
+//   loop: true,
+//   centerSlides: true,
+//   fadeEffect: true,
+//   grabCursor: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     dynamicBullets: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   breakpoints: {
+//     0: {
+//       slidesPerView: 1,
+//     },
+//     520: {
+//       slidesPerView: 2,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//     },
+//     1000: {
+//       slidesPerView: 4,
+//     },
+//   },
+// });
